daa09592524af8000126efa96b082033adc0ea26
yh2576337518 <2576337518@qq.com>
2018-10-17 13:32:50 +0800

    Add files via upload

---
+++ GradientColorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GradientColorDemo
+//
+//  Created by 惠上科技 on 2018/10/17.
+//  Copyright © 2018年 惠上科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GradientColorDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GradientColorDemo
+//
+//  Created by 惠上科技 on 2018/10/17.
+//  Copyright © 2018年 惠上科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
